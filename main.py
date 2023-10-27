a8b6b5389c52efd09fa4361b4d8a1e14553f690e
ammarSura <ammarsura@gmail.com>
2023-10-27 18:42:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask import render_template
-from psycopg import Cursor
-from logging_config import logging_config
-import logging
-from psycopg_pool import ConnectionPool
 from db_utils import run_query, create_pool
+from user_methods import get_user, post_user
+import sys
 
 pool = create_pool()
 
@@ -12,20 +10,24 @@ app = Flask(__name__)
 app.config.from_pyfile('env.py')
 
 @app.route('/')
-def hello_world():
-    message = "Hello, World"
+def get_root():
+    message = "Airbnb"
     return render_template('index.html', message=message)
 
-@app.route('/crud/<name>/<name2>', methods=['GET'])
-def get_handler(name, name2):
-    argsDic = request.args.to_dict()
-    argsList = list(argsDic.values())
-    app.logger.info('Received this request method: %s', request.method)
-    app.logger.info('Received these query params: %s', argsList)
-    app.logger.info('Received these path params: %s, %s', name, name2)
-    return render_template('index.html', message='Query params: ' + ' '.join(map(str, argsList)))
+@app.route('/users/<id>', methods=['GET'])
+def get_user_handler(id):
+    user = run_query(pool, lambda cur: get_user(cur, { 'id': id }))
+    return render_template('index.html', message='Query params: ' + str(user['id']))
+@app.route('/login', methods=['POST'])
+def login_handler():
+    args_dic = request.json
+    result = run_query(pool, lambda cur: post_user(cur, args_dic))
+    if(result == None):
+        abort(400, 'Missing param')
+    else:
+        return render_template('index.html', message=result)
 
-@app.route('/crud', methods=['POST'])
+@app.route('/host', methods=['GET'])
 def post_handler():
     app.logger.info('Received this request method: %s', request.method)
     app.logger.info('Received this request body: %s', request.get_json().get('name'))
