169205a52df5e47c2a4a0821740b15bc87d3feb2
ammarSura <ammarsura@gmail.com>
2023-10-28 12:15:04 +0530

    feat: return values as dict

--- db_utils.py
+++ db_utils.py
@@ -1,19 +1,25 @@
 from psycopg import Cursor
 from psycopg_pool import ConnectionPool
 from typing import Callable
-import logging
 from env import DB_URL
+from psycopg.rows import dict_row
+
+def set_missing_params_to_none(args_dic: dict, required_params: list):
+    for param in required_params:
+        if param not in args_dic:
+            args_dic[param] = None
 
 def run_query(pool: ConnectionPool, query: Callable[[], Cursor]):
     with pool.connection() as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(row_factory=dict_row) as cur:
             return query(cur)
+
 
 def create_pool():
     pool = ConnectionPool(
         conninfo=DB_URL
     )
     pool.wait()
-    logging.info('DB initiated')
+    print('Pool created')
     return pool
 
