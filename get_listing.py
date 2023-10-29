22c4e38a7f49a759955d0151446487d4ef64371c
ammarSura <ammarsura@gmail.com>
2023-10-29 11:53:44 +0530

    refac: use select_query

--- get_listing.py
+++ get_listing.py
@@ -1,25 +1,10 @@
 from psycopg import Connection, Cursor
-from db_utils import run_query, set_missing_params_to_none
-
-def get_listing_query(cur: Cursor, args_dic: dict):
-    cur.execute("""
-        SELECT * FROM listing
-        WHERE (%(id)s::INTEGER IS NULL OR id = %(id)s)
-        ORDER BY created_at DESC
-        LIMIT COALESCE(%(count)s, 1);
-    """,
-    args_dic
-    )
-    result = None
-    if(args_dic['count']):
-        result = cur.fetchall()
-    else:
-        result = cur.fetchone()
-    return result
+from db_utils import run_query, select_query, set_missing_params_to_none
+from psycopg import sql
 
 def get_listing(pool: Connection, args_dic: dict) -> int or None:
-    set_missing_params_to_none(args_dic, [
-        'id',
-        'count'
-    ])
-    return run_query(pool, lambda cur: get_listing_query(cur, args_dic))
+    count = None
+    if('count' in args_dic):
+        count = args_dic['count']
+        del args_dic['count']
+    return run_query(pool, lambda cur: select_query(cur, 'listing', args_dic, count))
