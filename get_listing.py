0ff2d2b9f5704c9e84b8bcbf664103fad42e302b
ammarSura <ammarsura@gmail.com>
2023-10-30 13:07:46 +0530

    fix: bug

--- get_listing.py
+++ get_listing.py
@@ -5,7 +5,6 @@ from db_utils import run_query, select_query, set_missing_params_to_none
 
 def get_listing(pool: Connection, args_dic: dict) -> int or None:
     count = None
-    if ('count' in args_dic):
     extra_query = None
     page = None
     if('count' in args_dic):
