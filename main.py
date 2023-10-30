4c33a0cd99df334a17e8389f57d115ee3cf8f2d2
Tashi <paritoshpanda21@gmail.com>
2023-10-30 07:37:52 +0530

    reformat

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-from flask import Flask, request, abort
-from flask import render_template
+from flask import Flask, abort, render_template, request
+
 from db_utils import create_pool
 from get_host import get_host
 from get_listing import get_listing
 from get_user import get_user
 from post_host import post_host
 from post_user import post_user
-import sys
 
 pool = create_pool()
 
 app = Flask(__name__)
 app.config.from_pyfile('env.py')
 
+
 @app.route('/')
 def get_root():
     message = "Airbnb"
     return render_template('index.html', message=message)
 
+
 @app.route('/users/<id>', methods=['GET'])
 def get_user_handler(id):
     user = get_user(pool, {'id': id})
-    if(user == None):
+    if (user == None):
         abort(404, 'User not found')
     return render_template('index.html', message='Query params: ' + str(user['name']))
+
 
 @app.route('/login', methods=['POST'])
 def login_handler():
     args_dic = request.json
     result = post_user(pool, args_dic, app.logger)
-    if(result == None):
+    if (result == None):
         abort(400, 'Missing param')
     else:
         return render_template('index.html', message=result)
+
 
 @app.route('/host/<id>', methods=['GET'])
 def host_get_handler(id):
@@ -41,14 +44,16 @@ def host_get_handler(id):
     })
     return render_template('index.html', message='Query params: ' + str(host['id']))
 
+
 @app.route('/host', methods=['POST'])
 def host_post_handler():
     args_dic = request.json
     result = post_host(pool, args_dic, app.logger)
-    if(result == None):
+    if (result == None):
         abort(400, 'Missing param')
     else:
         return render_template('index.html', message=result)
+
 
 @app.route('/listing', methods=['GET'])
 def listings_get_handler():
@@ -57,6 +62,7 @@ def listings_get_handler():
     }
     listings = get_listing(pool, args_dic)
     return render_template('index.html', message='Query params: ' + str(len(listings)))
+
 
 @app.route('/listing/<id>', methods=['GET'])
 def listing_get_handler(id):
