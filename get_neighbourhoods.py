770e1258b04a56d6cec1bd8676d4368842de874d
ammarSura <ammarsura@gmail.com>
2023-10-30 12:53:30 +0530

    fix: type error

--- get_neighbourhoods.py
+++ get_neighbourhoods.py
@@ -1,5 +1,6 @@
 from psycopg import Connection
-from db_utils import run_query, select_query
+from psycopg_pool import ConnectionPool
+from db_utils import run_query
 
 def get_neighbhourhoods_query(cur: Connection) -> int or None:
     return cur.execute(
@@ -8,5 +9,5 @@ def get_neighbhourhoods_query(cur: Connection) -> int or None:
         ORDER BY neighborhood;
         """
     ).fetchall()
-def get_neighbourhoods(pool: Connection) -> int or None:
+def get_neighbourhoods(pool: ConnectionPool) -> int or None:
     return run_query(pool, lambda cur: get_neighbhourhoods_query(cur))
