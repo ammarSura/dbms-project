47f9e1cdd1e88eb9932783e56ea1217eaa618c18
ammarSura <ammarsura@gmail.com>
2023-10-31 13:20:24 +0530

    refac: budget query for best_listings

--- get_best_listings.py
+++ get_best_listings.py
@@ -11,14 +11,24 @@ def get_best_listing(pool: Connection, args_dic: dict) -> int or None:
         sql.Identifier('best_listings', 'price'),
         sql.Identifier('best_listings', 'room_type'),
         sql.Identifier('best_listings', 'rating'),
+        sql.Identifier('best_listings', 'reviewer_name'),
+        sql.Identifier('best_listings', 'comments'),
+        sql.Identifier('best_listings', 'neighbourhood'),
     ]
     count = None
     extra_query = None
+    order_by = None
     if('count' in args_dic):
         count = args_dic['count']
         del args_dic['count']
-    if('extra_query' in args_dic):
-        extra_query = args_dic['extra_query']
-        del args_dic['extra_query']
-
-    return run_query(pool, lambda cur: select_query(cur, fields, 'best_listings', args_dic, extra_query, count))
+    if('is_budget' in args_dic):
+        is_budget = args_dic['is_budget']
+        del args_dic['is_budget']
+        if(is_budget):
+            extra_query = {
+                'query_lst': [
+                    sql.SQL('\nWHERE best_listings.price < 100'),
+                ]
+            }
+            order_by = 'price'
+    return run_query(pool, lambda cur: select_query(cur, fields, 'best_listings', args_dic, extra_query, count, None, order_by))
